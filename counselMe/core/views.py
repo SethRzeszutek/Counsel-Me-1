c9db153fb12d55a33ea97c0c060d6b81c57e45f4
Aaron Goodrich <aarongoodrich3@gmail.com>
2019-04-16 20:12:50 -0400

    most basic view ever to get us started

--- counselMe/core/views.py
+++ counselMe/core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Patient
 
-# Create your views here.
+def home(request):
+    x='hello world'
+    if request.method == 'GET':
+        return render(request, 'home.html', {'look_at_me': x})
