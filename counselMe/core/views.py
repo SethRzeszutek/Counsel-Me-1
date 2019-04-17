a41ea42c47a601cf704f663063b5ef65526b5f1c
Seth Rzeszutek <sethrzeszutek@gmail.com>
2019-04-17 18:00:47 -0400

    Added a login view, and tried to implement a CSS folder in the static folder. I tried to get the form action to redirect to the home page, but have not successfully got it yet.

--- counselMe/core/views.py
+++ counselMe/core/views.py
@@ -4,4 +4,4 @@ from .models import Patient
 def home(request):
 	x='hello world'
 	if request.method == 'GET':
-		return render(request, 'home.html', {'look_at_me': x})
+		return render(request, 'login.html', {'look_at_me': x})
