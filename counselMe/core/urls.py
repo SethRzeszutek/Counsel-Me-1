1d8bfa6eb451bfbdebbb07944b9bc7ebf509f977
Seth Rzeszutek <sethrzeszutek@gmail.com>
2019-04-18 15:12:58 -0400

    A whole lot of stuff happened here, most of it was cosmetic from a bit of css to converting spaces to tabs. However, I am currently in the middle of refactoring how the update and delete features work. They are in the middle of being used as modals rather than their own separate page.

--- counselMe/core/urls.py
+++ counselMe/core/urls.py
@@ -3,10 +3,12 @@ from core import views
 
 app_name = 'core'
 urlpatterns = [
-    re_path(r'^$',views.PatientList.as_view(),name='patient_index'),
-    re_path(r'^patient/create/$',views.PatientCreate.as_view(),name='patient_create'),
-    re_path(r'^patient/(?P<pk>[0-9]+)/$',views.PatientDetail.as_view(),name='patient_detail'),
-    re_path(r'^patient/(?P<pk>[0-9]+)/update/$',views.PatientUpdate.as_view(),name='patient_update'),
-    re_path(r'^patient/(?P<pk>[0-9]+)/remove/$',views.PatientDelete.as_view(),name='patient_delete'),
+	re_path(r'^$',views.login,name='login'),
+	re_path(r'^home/$',views.home,name='home'),
+	re_path(r'^patient/$',views.PatientList.as_view(),name='patient_index'),
+	re_path(r'^patient/create/$',views.PatientCreate.as_view(),name='patient_create'),
+	re_path(r'^patient/(?P<pk>[0-9]+)/$',views.PatientDetail.as_view(),name='patient_detail'),
+	re_path(r'^patient/(?P<pk>[0-9]+)/update/$',views.PatientUpdate.as_view(),name='patient_update'),
+	re_path(r'^patient/(?P<pk>[0-9]+)/remove/$',views.PatientDelete.as_view(),name='patient_delete'),
 ]
 
--- counselMe/core/views.py
+++ counselMe/core/views.py
@@ -9,37 +9,40 @@ from .forms import PatientCreateForm
 
 from .models import Patient
 
-def home(request):
-    x='hello world'
-    if request.method == 'GET':
-        return render(request, 'login.html', {'look_at_me': x})
+def login(request):
+	x='hello world'
+	if request.method == 'GET':
+		return render(request, 'login.html', {'look_at_me': x})
 
+def home(request):
+	if request.method == 'GET':
+		return render(request, 'home.html')
 class PatientList(ListView):
-    model = Patient
-    template_name = 'patient/index.html'
-    context_object_name = 'patients'
+	model = Patient
+	template_name = 'patient/index.html'
+	context_object_name = 'patients'
 
 class PatientDetail(DetailView):
-    model = Patient
-    template_name = "patient/detail.html"
-    context_object_name = 'patient'
+	model = Patient
+	template_name = "patient/detail.html"
+	context_object_name = 'patient'
 
 class PatientCreate(CreateView):
-    model = Patient
-    success_url = reverse_lazy('core:patient_index')
-    form_class = PatientCreateForm
-    template_name = 'patient/create.html'
-    context_object_name = 'patient'
+	model = Patient
+	success_url = reverse_lazy('core:patient_index')
+	form_class = PatientCreateForm
+	template_name = 'patient/create.html'
+	context_object_name = 'patient'
 
 class PatientUpdate(UpdateView):
-    model = Patient
-    success_url = reverse_lazy('core:patient_index')
-    fields = ['name', 'address', 'favorite_animal', 'insurance',]
-    template_name = 'patient/update.html'
-    context_object_name = 'patient'
+	model = Patient
+	success_url = reverse_lazy('core:patient_index')
+	fields = ['name', 'address', 'favorite_animal', 'insurance',]
+	template_name = 'patient/update.html'
+	context_object_name = 'patient'
 
 class PatientDelete(DeleteView):
-    model = Patient
-    success_url = reverse_lazy('core:patient_index')
-    template_name = 'patient/delete.html'
-    context_object_name = 'patient'
+	model = Patient
+	success_url = reverse_lazy('core:patient_index')
+	template_name = 'patient/delete.html'
+	context_object_name = 'patient'
