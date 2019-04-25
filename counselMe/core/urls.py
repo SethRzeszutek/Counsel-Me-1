f331bc90bfad7cf48bbccb64c99b03a895537a54
Seth Rzeszutek <sethrzeszutek@gmail.com>
2019-04-25 14:46:09 -0400

    Logging in technically works after creating a user, however the URLs for Login and Logout are broken and I am still figuring that out.

--- counselMe/core/urls.py
+++ counselMe/core/urls.py
@@ -1,10 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf.urls import re_path
+from django.views.generic.base import TemplateView
 from core import views
 
 app_name = 'core'
 urlpatterns = [
-	re_path(r'^$',views.login,name='login'),
-	re_path(r'^home/$',views.home,name='home'),
+	#re_path(r'^$',views.landing,name='landing'),
+	#re_path(r'^$',views.login,name='login'),
+	#re_path(r'^$',views.home,name='home'),
+	path('', TemplateView.as_view(template_name='home.html'), name='home'),
 	re_path(r'^planner/$',views.planner,name='planner'),
 
 	re_path(r'^patient/$',views.PatientList.as_view(),name='patient_index'),
@@ -18,5 +23,9 @@ urlpatterns = [
 	re_path(r'^form/(?P<pk>[0-9]+)/view/$',views.FormView.as_view(),name='form_view'),
 	re_path(r'^form/(?P<pk>[0-9]+)/edit/$',views.FormEdit.as_view(),name='form_edit'),
 	re_path(r'^form/(?P<pk>[0-9]+)/remove/$',views.FormDelete.as_view(),name='form_delete'),
+	
+	path('admin/', admin.site.urls),
+	path('accounts/', include('django.contrib.auth.urls')), # new
+
 ]
 
