f226f828230d98617693418260ec8eb34aab5f63
Seth Rzeszutek <sethrzeszutek@gmail.com>
2019-05-02 16:09:09 -0400

    Its broken... User does not work like I thought.

--- counselMe/core/models.py
+++ counselMe/core/models.py
@@ -1,8 +1,31 @@
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
 from django.utils import timezone
 from django.db import models
+
+class User(AbstractUser):
+
+	# Define the extra fields
+	# related to User here
+	first_name = models.CharField(_('First Name of User'),
+							blank = True, max_length = 20)
+
+	last_name = models.CharField(_('Last Name of User'),
+							blank = True, max_length = 20)
+
+# More User fields according to need
+	# define the custom permissions
+	# related to User.
+	class Meta:
+
+		permissions = (
+			("can_add_users", "Add User"),
+			("can_use_form", "Able to use Form"),
+			("can_view_patient", "Able to view Patient"),
+			("can_edit_patient", "Able to edit Patient"),
+		)
+
 
 class Patient(models.Model):
 	ANIMAL_CHOICES = (
@@ -36,25 +59,3 @@ class Form(models.Model):
 	def __str__(self):
 		return self.name
 
-# User class
-class NewUser(AbstractUser):
-
-	# Define the extra fields
-	# related to User here
-	first_name = models.CharField(_('First Name of User'),
-							blank = True, max_length = 20)
-
-	last_name = models.CharField(_('Last Name of User'),
-							blank = True, max_length = 20)
-
-# More User fields according to need
-	# define the custom permissions
-	# related to User.
-	class Meta:
-
-		permissions = (
-			("can_add_users", "Add User"),
-			("can_use_form", "Able to use Form"),
-			("can_view_patient", "Able to view Patient"),
-			("can_edit_patient", "Able to edit Patient"),
-		)
